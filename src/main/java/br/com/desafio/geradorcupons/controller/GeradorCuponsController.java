d014bd07f5285071486f680dca4a7b0c561e02e8
matheus <matheusoliveira1991@hotmail.com>
2025-12-01 17:30:05 -0300

    created endpoint ok

--- src/main/java/br/com/desafio/geradorcupons/controller/GeradorCuponsController.java
+++ src/main/java/br/com/desafio/geradorcupons/controller/GeradorCuponsController.java
@@ -3,13 +3,20 @@ package br.com.desafio.geradorcupons.controller;
 
 import br.com.desafio.geradorcupons.dto.CouponRequest;
 import br.com.desafio.geradorcupons.dto.response.CouponResponse;
+import br.com.desafio.geradorcupons.dto.response.CouponResponseError;
 import br.com.desafio.geradorcupons.entity.CouponEntity;
+import br.com.desafio.geradorcupons.exception.CouponInvalidException;
 import br.com.desafio.geradorcupons.factory.CouponEntityFactory;
 import br.com.desafio.geradorcupons.service.CouponService;
+import br.com.desafio.geradorcupons.utils.CouponUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("/api/v1/cupom/")
@@ -21,14 +28,28 @@ public class GeradorCuponsController {
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody(required = true) CouponRequest couponRequest) {
         try {
+            final LocalDate today = LocalDate.now();
+            final String promoCodeRefactor = CouponUtils.formatCouponCode(couponRequest.getCode());
+            couponRequest.setCode(promoCodeRefactor);
+
+            if (couponRequest.getExpirationDate().isBefore(today)) {
+                throw new CouponInvalidException("Expiration value must be valid today or later");
+            }
+
+            double min_discount = 0.5;
+            if (couponRequest.getDiscountValue() < min_discount) {
+                throw new CouponInvalidException("Discount value must be larger than R$ 0.5");
+            }
+
             CouponEntity couponEntity = CouponEntityFactory.createFrom(couponRequest);
 
             couponService.save(couponEntity);
 
-            return ResponseEntity.ok(new CouponResponse("Your coupon has been created successfully"));
+            return ResponseEntity.status(CREATED.value())
+                    .body(new CouponResponse("Your coupon has been created successfully"));
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).build();
-
+            return ResponseEntity.status(BAD_REQUEST.value())
+                    .body(new CouponResponseError(e.getMessage()));
         }
     }
 
