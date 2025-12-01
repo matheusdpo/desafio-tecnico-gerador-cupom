8012b8a9dbd5554a273b68532868a50388f4ee72
matheus <matheusoliveira1991@hotmail.com>
2025-12-01 18:01:54 -0300

    create couponby coupon entity

--- src/main/java/br/com/desafio/geradorcupons/factory/CouponEntityFactory.java
+++ src/main/java/br/com/desafio/geradorcupons/factory/CouponEntityFactory.java
@@ -3,6 +3,8 @@ package br.com.desafio.geradorcupons.factory;
 import br.com.desafio.geradorcupons.dto.CouponRequest;
 import br.com.desafio.geradorcupons.entity.CouponEntity;
 
+import java.time.LocalDateTime;
+
 public class CouponEntityFactory {
 
     public static CouponEntity createFrom(CouponRequest couponRequest) {
@@ -13,6 +15,20 @@ public class CouponEntityFactory {
                 .expirationDate(couponRequest.getExpirationDate())
                 .published(couponRequest.getPublished())
                 .couponDeleted(false)
+                .build();
+    }
+
+    public static CouponEntity createFrom(CouponEntity couponEntity) {
+        return new CouponEntity.Builder()
+                .id(couponEntity.getId())
+                .code(couponEntity.getCode())
+                .description(couponEntity.getDescription())
+                .discountValue(couponEntity.getDiscountValue())
+                .expirationDate(couponEntity.getExpirationDate())
+                .published(couponEntity.isPublished())
+                .couponDeleted(true)
+                .createdAt(couponEntity.getCreatedAt())
+                .deletedAt(LocalDateTime.now())
                 .build();
     }
 }
