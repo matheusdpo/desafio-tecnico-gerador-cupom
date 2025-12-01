3b193276814717a75e6ca18b2dd150b3e9c066dc
matheus <matheusoliveira1991@hotmail.com>
2025-12-01 18:39:34 -0300

    find by id and status

--- src/main/java/br/com/desafio/geradorcupons/repository/CouponRepository.java
+++ src/main/java/br/com/desafio/geradorcupons/repository/CouponRepository.java
@@ -1,6 +1,7 @@
 package br.com.desafio.geradorcupons.repository;
 
 import br.com.desafio.geradorcupons.entity.CouponEntity;
+import br.com.desafio.geradorcupons.enums.StatusCouponEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface CouponRepository extends JpaRepository<CouponEntity, Long> {
-    Optional<CouponEntity> findByCodeAndCouponDeletedFalse(String code);
+    Optional<CouponEntity> findByIdAndStatus(Long id, StatusCouponEnum status);
 }
