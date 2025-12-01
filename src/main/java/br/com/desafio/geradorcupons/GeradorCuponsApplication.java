3e8e089ad46096ad60c7ae0f3b2f57952791864b
matheus <matheusoliveira1991@hotmail.com>
2025-12-01 15:29:06 -0300

    first commit: added spring and dependencies

---
+++ src/main/java/br/com/desafio/geradorcupons/GeradorCuponsApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.geradorcupons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorCuponsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorCuponsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafio/geradorcupons/GeradorCuponsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.geradorcupons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorCuponsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
