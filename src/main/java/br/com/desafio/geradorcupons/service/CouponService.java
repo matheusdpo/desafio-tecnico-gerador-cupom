83330a71156bf2997ac59108965bb2915fe32567
matheus <matheusoliveira1991@hotmail.com>
2025-12-01 18:02:10 -0300

    bug fixed: delete msut be false

--- src/main/java/br/com/desafio/geradorcupons/service/CouponService.java
+++ src/main/java/br/com/desafio/geradorcupons/service/CouponService.java
@@ -5,14 +5,19 @@ import br.com.desafio.geradorcupons.repository.CouponRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class CouponService {
 
     @Autowired
     private CouponRepository couponRepository;
 
-    public CouponEntity save(CouponEntity couponEntity) {
+    public CouponEntity saveOrUpdate(CouponEntity couponEntity) {
         return couponRepository.save(couponEntity);
     }
 
+    public Optional<CouponEntity> findByCodeAndCouponDeletedFalse(String code) {
+        return couponRepository.findByCodeAndCouponDeletedFalse(code);
+    }
 }
